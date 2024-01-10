db13e83f04fc8f429c63408ef88f90a60ede4226
ObinnaMartin <153989044+ObinnaMartin@users.noreply.github.com>
2024-01-10 13:38:30 +0100

    Add files via upload

---
+++ DataScienceEcosystem.ipynb..py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In the notebook, Data Science Tools and Ecosystem are Summarized.
+
+# **Objectives:**
+# * Scala
+# * C++
+# * Python
+# * Julia
+# * R
+
+# Some of the popular langauages that Data Scientists use are:
+# 1. Python
+# 2. R language
+# 3. SQL lanaguage
+# 4. Javascript
+# 5. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Scientific Computing libraries in Python
+# 2. Visualization libraries in Python
+# 3. High Level-Machine Learning
+# 4. Deep Learning libraries.
+
+# |Data Science Tools|
+# |------------------|
+# |Watson Studio|
+# |Jupyter Notebook|
+# |RStudio|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[2]:
+
+
+#This is a simple arithmetic expression to multiply then add integers#
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200minutes to hours by dividing by 60#
+200/60
+
+
+# ## Author
+# Nwokoro Obinna Martin
+
+# In[ ]:
+
+
+
+
